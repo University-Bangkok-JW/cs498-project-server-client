745ab811b42558821e119294c34b1a0bd905f1b2
JetsadaWijit <jetsadawijit@outlook.com>
2025-03-12 11:23:06 +0000

    Change logic to pg db

--- src/db/database.js
+++ src/db/database.js
@@ -1,8 +1,15 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "data/users.sqlite",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME || "mydb",
+  process.env.DB_USER || "postgres",
+  process.env.DB_PASSWORD || "1234",
+  {
+    host: process.env.DB_HOST || "db",
+    dialect: "postgres",
+    port: process.env.DB_PORT || 5432,
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
